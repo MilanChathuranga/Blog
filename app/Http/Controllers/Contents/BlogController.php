4b59bd8ee4f86b3e717bbf583c3374eb181790d2
MilanChathuranga <41052536+MilanChathuranga@users.noreply.github.com>
2023-02-17 11:00:23 +0530

    Global and Local Scopes Implement

--- app/Http/Controllers/Contents/BlogController.php
+++ app/Http/Controllers/Contents/BlogController.php
@@ -8,6 +8,7 @@ use App\Models\Post;
 use App\Models\PostCategory;
 use App\Models\PostComment;
 use App\Models\PostTag;
+use App\Models\Scopes\PostActiveScope;
 use App\Models\Tag;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -20,17 +21,29 @@ class BlogController extends Controller
     public function index()
     {
 
+//  Using Local Scope get posts where author_id equals to 2
+//        dd(Post::author()->get());
+//  Using Local Scope and removing Global Scope to get posts where unpublished
+//        dd(Post::withoutGlobalScope(PostActiveScope::class)->deactive()->get());
+
         $comments = PostComment::where('post_id', 1)->whereParentId(0)->get();
 //        check user permission
         $user = Auth::user();
         if ($user->can('publish post')) {
             $posts = Cache::remember('post', now()->day, function () {
-                return Post::paginate(6);
+
+//  Removing Global Scopes, can use withoutGlobalScope to remove global scope
+                return Post::withoutGlobalScope(PostActiveScope::class)->paginate(6);
+
+//  Removing Global Scopes,Can use this format for without create Scope class
+//                Post::withoutGlobalScope('active')->get();
             });
-            return view('contents.blog', compact('posts','comments'));
+            return view('contents.blog', compact('posts', 'comments'));
 
         } else {
-            $posts = Post::where('published', 1)->paginate(6);
+
+//  Using  Global Scopes, returns all post where published = 1
+            $posts = Post::paginate(6);
             return view('contents.blog', compact('posts', 'comments'));
         }
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\PostActiveScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,5 +21,31 @@ class Post extends Model
         'content',
         'published_at',
     ];
+
+
+//  this method for Local Scope where get details about author_id equals to 2
+    public function scopeAuthor($query)
+    {
+        $query->where('author_id',  2);
+    }
+
+//  this method for Local Scope where get details about unpublished post
+    public function scopeDeactive($query)
+    {
+        $query->where('published',  0);
+    }
+
+
+
+//  this method for Global Scope
+    protected static function booted()
+    {
+        static::addGlobalScope(new PostActiveScope);
+//        Can use this format without create Scope class
+
+//        static::addGlobalScope('active', function (Builder $builder) {
+//            $builder->where('published', '=', 1);
+//        });
+    }
 
 }
